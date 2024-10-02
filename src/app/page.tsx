9c1d2db07cf783f99e3d46ea51563dedc3f7a99d
gleysonemilio <gleyson.emilio@outlook.com>
2024-10-01 23:44:56 -0300

    feat(): remove console

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,16 @@
 import { NavBar } from '@/components/NavBar'
 import { useAppContext } from './hooks/AppContext'
 import Tiptap from '@/components/EditorContent'
+import { useEffect, useMemo, useState } from 'react'
 
 export default function Home() {
-  const { content } = useAppContext()
+  const { inforPage } = useAppContext()
 
   return (
     <div className=" bg-zinc-900 w-[100%] h-[100%] mx-auto min-h-[900px] grid grid-cols-[16rem_1fr]">
       <NavBar />
-      <main className="max-m-[700px] mx-auto pt-16">{content && <Tiptap content={content} />}</main>
+
+      <main className="max-m-[700px] mx-auto pt-16">{inforPage.content ? <Tiptap /> : <></>}</main>
     </div>
   )
 }
