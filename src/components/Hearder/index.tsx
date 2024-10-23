a046d3410b72bf6317d70060dd541c160c6fef3c
gleysonemilio <gleyson.emilio@outlook.com>
2024-10-23 00:13:28 -0300

    feat (index): SheetTrigger

---
+++ src/components/Hearder/index.tsx
@@ -0,0 +1,43 @@
+import { AlignJustify } from 'lucide-react'
+
+import { Button } from '@/components/ui/button'
+import Image from 'next/image'
+
+import LogoBrain from '@/assets/logo-brain-2.png'
+
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger
+} from '@/components/ui/sheet'
+import { SearchBook } from '../SeachBook'
+
+export const Hearder = () => {
+  return (
+    <div className="flex gap-2 justify-between p-2">
+      <div>
+        <Image src={LogoBrain} width={70} height={50} alt="Picture of the author" />
+      </div>
+      <div>
+        <Sheet>
+          <SheetTrigger className="w-[100%]">
+            <Button variant="outline">
+              <AlignJustify color="#7b7b81" width={16} />
+            </Button>
+          </SheetTrigger>
+          <SheetContent>
+            <SheetHeader>
+              <SheetTitle>Select Book</SheetTitle>
+              <SheetDescription>
+                <SearchBook />
+              </SheetDescription>
+            </SheetHeader>
+          </SheetContent>
+        </Sheet>
+      </div>
+    </div>
+  )
+}
