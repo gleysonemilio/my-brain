13c0acda90ec6a935d80e1947ffe33eac084b2eb
gleysonemilio <gleyson.emilio@outlook.com>
2024-10-23 00:11:13 -0300

    feat (): add header and asider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,24 @@
 import { NavBar } from '@/components/NavBar'
 import { useAppContext } from './hooks/AppContext'
 import Tiptap from '@/components/EditorContent'
+import { Hearder } from '@/components/Hearder'
 
 export default function Home() {
   const { inforPage } = useAppContext()
 
   return (
     <div className="flex bg-zinc-900 w-[100%] h-[100%] md:mx-auto min-h-[900px] md:grid md:grid-cols-[16rem_1fr]">
-      <NavBar />
-      <main className="max-m-[700px] mx-auto pt-16 p-3 bg-zinc-900" >{inforPage.content ? <Tiptap /> : <span></span>}</main>
+      <header className="sm:fixed sm:direction-normal sm:w-[100%] sm:bg-[#0b0b0c] sm:z-10 md:hidden lg:hidden xl:hidden">
+        <Hearder />
+      </header>
+
+      <aside className="sm:hidden md:flex lg:flex xl:flex">
+        <NavBar />
+      </aside>
+
+      <main className="w-[100%] max-m-[700px] mx-auto pt-16 p-3 bg-zinc-900">
+        {inforPage.content ? <Tiptap /> : <span></span>}
+      </main>
     </div>
   )
 }
