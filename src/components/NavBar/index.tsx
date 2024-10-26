2f4b1a6f73d1cd03ac1502f933dce202a8d07671
gleysonemilio <gleyson.emilio@outlook.com>
2024-10-25 22:10:46 -0300

    feat (): new logo

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,12 +1,12 @@
 import Image from 'next/image'
 
-import LogoBrain from '@/assets/logo-brain-2.png'
+import LogoBrain from '@/assets/logo-brain-1.png'
 import { SearchBook } from '../SeachBook'
 
 export const NavBar = () => {
   return (
     <div className="bg-[#0b0b0c] border-r-zinc-700 p-3 ">
-      <Image src={LogoBrain} width={70} height={50} alt="Picture of the author" />
+      <Image src={LogoBrain} width={30} height={30} alt="Picture of the author" />
 
       <div className="flex content-center gap-2 justify-center items-center mt-3">
         <SearchBook />
