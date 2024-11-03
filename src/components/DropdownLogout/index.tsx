0918f79757a7be3bbd71477cbd748195474c54cb
gleysonemilio <gleyson.emilio@outlook.com>
2024-11-03 12:30:32 -0300

    feat (): user new component

--- src/components/DropdownLogout/index.tsx
+++ src/components/DropdownLogout/index.tsx
@@ -10,9 +10,11 @@ import {
   DropdownMenuTrigger
 } from '@/components/ui/dropdown-menu'
 import { deleteCookie } from 'cookies-next'
-import { EllipsisIcon, EllipsisVerticalIcon } from 'lucide-react'
+import { EllipsisIcon, EllipsisVerticalIcon, LogInIcon, User2Icon } from 'lucide-react'
 import Image from 'next/image'
 import { useRouter } from 'next/navigation'
+
+import { ComponentProfile } from '../ComponentProfile'
 
 export const DropdownLogout = () => {
   const router = useRouter()
@@ -23,36 +25,28 @@ export const DropdownLogout = () => {
   const decodedImageUrl = (name: string) => decodeURIComponent(name as string)
 
   const handleLogOut = () => {
-    ['photoURL', 'photoURL', 'uid', 'email', 'displayName'].forEach((key) => deleteCookie(key))
+    ;['photoURL', 'photoURL', 'uid', 'email', 'displayName'].forEach((key) => deleteCookie(key))
 
     return router.push('/login')
   }
 
   return (
-    <DropdownMenu>
+    <DropdownMenu >
       <DropdownMenuTrigger>
-        <div className="flex gap-3 justify-center items-center bg-zinc-900 p-3 rounded-lg">
-          <Image
-            src={decodedImageUrl(photoURL)}
-            alt="photo profile"
-            className="rounded-lg"
-            width={40}
-            height={40}
-          />
-
-          <div className="flex flex-col items-start gap-1">
-            <small className="text-sm font-bold leading-none">{decodedImageUrl(displayName)}</small>
-            <p className="text-xs text-muted-foreground text-zinc-700"> {decodedImageUrl(email)}</p>
-          </div>
-
-          <EllipsisVerticalIcon size={40} />
-        </div>
+        <ComponentProfile icon background />
       </DropdownMenuTrigger>
       <DropdownMenuContent>
-        <DropdownMenuLabel>My Account</DropdownMenuLabel>
+        <ComponentProfile />
+
         <DropdownMenuSeparator />
-        <DropdownMenuItem>Profile</DropdownMenuItem>
-        <DropdownMenuItem onClick={handleLogOut}>Log out</DropdownMenuItem>
+        <DropdownMenuItem>
+          <User2Icon />
+          Profile
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={handleLogOut}>
+          <LogInIcon />
+          Log out
+        </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
   )
