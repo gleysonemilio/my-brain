4ee4e137f297646553d2d9a84a6b35c0120c945a
gleysonemilio <gleyson.emilio@outlook.com>
2024-11-03 11:32:29 -0300

    feat (): fix dimersion

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -2,14 +2,16 @@ import LogoBrain from '@/assets/logo-brain-1.png'
 import Image from 'next/image'
 
 import { SearchBook } from '../SeachBook'
+import { DropdownLogout } from '../DropdownLogout'
 
 export const NavBar = () => {
   return (
-    <div className="border-r-zinc-700 bg-[#0b0b0c] p-3">
+    <div className="h-[100vh] border-r-zinc-700 bg-[#0b0b0c] p-3 gap-5">
       <Image src={LogoBrain} width={70} height={30} alt="Picture of the author" />
 
-      <div className="mt-3 flex content-center items-center justify-center gap-2">
+      <div className="h-[95%] flex flex-col mt-3 gap-3">
         <SearchBook />
+        <DropdownLogout />
       </div>
     </div>
   )
