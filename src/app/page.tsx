ad1ae323108a7299ff3c7f79af6550cc0ca39c76
gleysonemilio <gleyson.emilio@outlook.com>
2024-11-03 11:30:57 -0300

    feat (): save cookies context

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,22 @@
 import Tiptap from '@/components/EditorContent'
 import { Hearder } from '@/components/Hearder'
 import { NavBar } from '@/components/NavBar'
+import { getCookie, getCookies } from 'cookies-next'
+import { useEffect } from 'react'
 
 import { useAppContext } from './hooks/AppContext'
 
 export default function Home() {
-  const { inforPage } = useAppContext()
+  const { inforPage, account, setAccount } = useAppContext()
+
+  useEffect(() => {
+    if (account?.uid) return
+
+    setAccount(getCookies())
+  }, [])
 
   return (
-    <div className="flex h-[100%] min-h-[900px] w-[100%] bg-zinc-900 md:mx-auto md:grid md:grid-cols-[16rem_1fr]">
+    <div className="flex h-[100vh] w-[100%] bg-zinc-900 md:mx-auto md:grid md:grid-cols-[16rem_1fr]">
       <header className="sm:fixed sm:z-10 sm:w-[100%] sm:bg-[#0b0b0c] sm:direction-normal md:hidden lg:hidden xl:hidden">
         <Hearder />
       </header>
