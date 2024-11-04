154e5d38a540ff376693f4c630e6248878446086
gleysonemilio <gleyson.emilio@outlook.com>
2024-11-03 22:32:07 -0300

    feat (): css

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,26 +8,34 @@ const config: Config = {
 		"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
 	],
 	theme: {
-		screens: {
-			'sm': '240px',
-			'md': '768px',
-			'lg': '1024px',
-			'xl': '1280px',
-		  },
-		extend: {
-			colors: {
-				background: 'var(--background)',
-				foreground: 'var(--foreground)'
-			},
-			borderRadius: {
-				lg: 'var(--radius)',
-				md: 'calc(var(--radius) - 2px)',
-				sm: 'calc(var(--radius) - 4px)'
-			}
-		},     
-	
-		
-	},
+    	screens: {
+    		sm: '240px',
+    		md: '768px',
+    		lg: '1024px',
+    		xl: '1280px'
+    	},
+    	extend: {
+    		colors: {
+    			background: 'var(--background)',
+    			foreground: 'var(--foreground)',
+    			sidebar: {
+    				DEFAULT: 'hsl(var(--sidebar-background))',
+    				foreground: 'hsl(var(--sidebar-foreground))',
+    				primary: 'hsl(var(--sidebar-primary))',
+    				'primary-foreground': 'hsl(var(--sidebar-primary-foreground))',
+    				accent: 'hsl(var(--sidebar-accent))',
+    				'accent-foreground': 'hsl(var(--sidebar-accent-foreground))',
+    				border: 'hsl(var(--sidebar-border))',
+    				ring: 'hsl(var(--sidebar-ring))'
+    			}
+    		},
+    		borderRadius: {
+    			lg: 'var(--radius)',
+    			md: 'calc(var(--radius) - 2px)',
+    			sm: 'calc(var(--radius) - 4px)'
+    		}
+    	}
+    },
 	plugins: [
 		require('@tailwindcss/typography'),
 		require("tailwindcss-animate")],
