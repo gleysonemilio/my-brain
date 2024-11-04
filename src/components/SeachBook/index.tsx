37db070be8e21cc63998de422a42d2485ca2fd08
gleysonemilio <gleyson.emilio@outlook.com>
2024-11-03 22:33:14 -0300

    feat (SearchBook): adjutment

--- src/components/SeachBook/index.tsx
+++ src/components/SeachBook/index.tsx
@@ -4,29 +4,14 @@ import { useAppContext } from '@/app/hooks/AppContext'
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover'
-import {
-  Select,
-  SelectContent,
-  SelectGroup,
-  SelectItem,
-  SelectLabel,
-  SelectTrigger,
-  SelectValue
-} from '@/components/ui/select'
 import { createPage, getPagesOfUser, getUser } from '@/firebase/Api'
 import data from '@emoji-mart/data'
 import Picker from '@emoji-mart/react'
-import { AlignJustify, FilePlus2, FilesIcon } from 'lucide-react'
+import { AlignJustify, FilePlus2, FilesIcon, Plus } from 'lucide-react'
 import { useEffect, useState } from 'react'
 
 import { DropdownLogout } from '../DropdownLogout'
 import { Command, CommandGroup, CommandItem, CommandList } from '../ui/command'
-
-interface UserInterface {
-  name: string
-  email: string
-  id: string
-}
 
 export interface PagesInterface {
   id?: string
@@ -38,10 +23,9 @@ export interface PagesInterface {
 }
 
 export const SearchBook = () => {
-  const { setInforPage, resertInforPage, pages, setPages } = useAppContext()
+  const { setInforPage, resertInforPage, pages, setPages, account } = useAppContext()
 
   const [popoverOpen, setPopoverOpen] = useState(false)
-  const [listUsers, setListUsers] = useState<Array<UserInterface>>([])
   const [emoji, setEmoji] = useState('')
   const [createNewPageInfor, setCreateNewPageInfor] = useState<PagesInterface>({
     title: '',
@@ -52,17 +36,16 @@ export const SearchBook = () => {
   })
 
   useEffect(() => {
-    const fetchGetUserData = async () => {
-      const data = await getUser()
-      setListUsers(data as UserInterface[])
+    const fetchData = async () => {
+      await ApigetPagesOfUser(account.uid)
     }
 
-    fetchGetUserData()
-  }, [])
+    fetchData()
+  }, [account.uid])
 
-  const ApigetPagesOfUser = async (idUser: string) => {
+  const ApigetPagesOfUser = async (id: string) => {
     try {
-      const data = await getPagesOfUser(idUser)
+      const data = await getPagesOfUser(id)
 
       setPages(data as PagesInterface[])
     } catch (error) {
@@ -72,9 +55,10 @@ export const SearchBook = () => {
 
   const ApiSavePage = async () => {
     try {
-      await createPage({ ...createNewPageInfor, emoji })
+      await createPage({ ...createNewPageInfor, emoji, idUser: account.uid })
       setPopoverOpen(false)
-      ApigetPagesOfUser(createNewPageInfor.idUser)
+
+      ApigetPagesOfUser(account.uid)
     } catch (error) {
       console.error(error)
     }
@@ -85,7 +69,7 @@ export const SearchBook = () => {
       pages.length > 0 && (
         <Command className="h-full mt-3 rounded-lg shadow-md bg-zinc-200">
           <CommandList className="max-h-full">
-            <CommandGroup heading="Pages">
+            <CommandGroup>
               {pages.map((ele) => (
                 <div
                   key={ele.title}
@@ -97,9 +81,11 @@ export const SearchBook = () => {
                     }, 100)
                   }}
                 >
-                  <CommandItem className="ml-2 cursor-pointer">
+                  <CommandItem className="flex flex-row ml-2 cursor-pointer items-start">
                     <span className="mr-2 h-4 w-4">{ele?.emoji || <FilesIcon />}</span>
-                    <span>{ele.title}</span>
+                    <span className="scroll-m-20 text-sm font-semibold tracking-tight">
+                      {ele.title}
+                    </span>
                   </CommandItem>
                 </div>
               ))}
@@ -113,10 +99,10 @@ export const SearchBook = () => {
   return (
     <div className="h-full flex flex-col justify-between">
       <div className="flex gap-2">
-        <Select
+        {/* <Select
           onValueChange={(e) => {
-            ApigetPagesOfUser(e)
-            setCreateNewPageInfor({ ...createNewPageInfor, idUser: e })
+            ApigetPagesOfUser()
+            setCreateNewPageInfor({ ...createNewPageInfor })
           }}
         >
           <SelectTrigger className="w-[180px] overflow-hidden text-zinc-400">
@@ -140,49 +126,54 @@ export const SearchBook = () => {
               ))}
             </SelectGroup>
           </SelectContent>
-        </Select>
-
-        <Popover open={popoverOpen} onOpenChange={(e) => setPopoverOpen(e)}>
-          <PopoverTrigger asChild>
-            <Button variant="outline">
-              <FilePlus2 color="#7b7b81" width={16} />
-            </Button>
-          </PopoverTrigger>
-          <PopoverContent className="table w-80">
-            <div className="grid gap-4">
-              <div className="space-y-1">
-                <h4 className="font-medium leading-none">Creater New Page</h4>
-                <p className="text-muted-foreground text-sm">
-                  Select an emoji then enter the name of your book.
-                </p>
-              </div>
-              <div className="grid gap-3">
-                <div className="flex flex-row items-center gap-4">
-                  <span className="text-5xl">{emoji}</span>
-                  <Input
-                    id="width"
-                    className="h-8 w-[100%]"
-                    placeholder="Title"
-                    onChange={(e) =>
-                      setCreateNewPageInfor({ ...createNewPageInfor, title: e.target.value })
-                    }
-                  />
-                  <Button size={'sm'} onClick={() => ApiSavePage()}>
-                    save
-                  </Button>
-                </div>
-                <hr className="w-full border-zinc-800" />
-
-                <div>
-                  <p className="text-muted-foreground text-sm">select emoji</p>
-                  <Picker data={data} onEmojiSelect={(value: any) => setEmoji(value.native)} />
-                </div>
-              </div>
-            </div>
-          </PopoverContent>
-        </Popover>
+        </Select> */}
       </div>
-      <div className="h-full">{returnListPapers()}</div>
+      <div className="h-full">
+        <div className="flex flex-row justify-between items-center">
+          <span className="scroll-m-20 text-sm font-semibold tracking-tight">Books</span>
+
+          <Popover open={popoverOpen} onOpenChange={(e) => setPopoverOpen(e)}>
+            <PopoverTrigger asChild>
+              <button>
+                <Plus color="#7b7b81" width={16} />
+              </button>
+            </PopoverTrigger>
+            <PopoverContent className="table w-80">
+              <div className="grid gap-4">
+                <div className="space-y-1">
+                  <h4 className="font-medium leading-none">Creater New Page</h4>
+                  <p className="text-muted-foreground text-sm">
+                    Select an emoji then enter the name of your book.
+                  </p>
+                </div>
+                <div className="grid gap-3">
+                  <div className="flex flex-row items-center gap-4">
+                    <span className="text-5xl">{emoji}</span>
+                    <Input
+                      id="width"
+                      className="h-8 w-[100%]"
+                      placeholder="Title"
+                      onChange={(e) =>
+                        setCreateNewPageInfor({ ...createNewPageInfor, title: e.target.value })
+                      }
+                    />
+                    <Button size={'sm'} onClick={() => ApiSavePage()}>
+                      save
+                    </Button>
+                  </div>
+                  <hr className="w-full border-zinc-800" />
+
+                  <div>
+                    <p className="text-muted-foreground text-sm">select emoji</p>
+                    <Picker data={data} onEmojiSelect={(value: any) => setEmoji(value.native)} />
+                  </div>
+                </div>
+              </div>
+            </PopoverContent>
+          </Popover>
+        </div>
+        {returnListPapers()}
+      </div>
 
       <DropdownLogout />
     </div>
