245db1da505eeb4b37909415bef0c400e96ded6a
gleysonemilio <gleyson.emilio@outlook.com>
2024-11-04 19:25:31 -0300

    feat (): change router

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,14 +2,14 @@ import { NextRequest, NextResponse } from 'next/server'
 import { cookies } from 'next/headers'
 
 export function middleware(request: NextRequest) {
-    const protectedRoute = ['/']
+    const protectedRoute = ['/my-book']
     const cookieStore = cookies()
     const uid = cookieStore.get('uid')?.value
 
     const isProtectedRoute = protectedRoute.includes(request.nextUrl.pathname)
 
     if (isProtectedRoute && !uid) {
-        return NextResponse.redirect(new URL('/login', request.url))
+        return NextResponse.redirect(new URL('/', request.url))
     }
 
     return NextResponse.next()
