cd8037e8116f55326ae32803363ac3a7e6ec8cd7
gleysonemilio <gleyson.emilio@outlook.com>
2024-11-09 18:09:36 -0300

    feat (): add interface

--- src/components/EditorContent/CodeBlockComponent/index.tsx
+++ src/components/EditorContent/CodeBlockComponent/index.tsx
@@ -3,13 +3,27 @@ import React from 'react'
 
 import './CodeBlockComponent.css'
 
+interface NodeAttributes {
+  language: string
+}
+
+interface Node {
+  attrs: NodeAttributes
+}
+
+interface EditorObject {
+  node: Node
+  updateAttributes: (attrs: Partial<NodeAttributes>) => void 
+  extension: any 
+}
+
 export default ({
   node: {
     attrs: { language: defaultLanguage }
   },
   updateAttributes,
   extension
-}) => (
+}: EditorObject) => (
   <NodeViewWrapper className="code-block">
     <select
       contentEditable={false}
@@ -18,7 +32,7 @@ export default ({
     >
       <option value="null">auto</option>
       <option disabled>—</option>
-      {extension.options.lowlight.listLanguages().map((lang, index) => (
+      {extension.options.lowlight.listLanguages().map((lang: any, index: any) => (
         <option key={index} value={lang}>
           {lang}
         </option>
