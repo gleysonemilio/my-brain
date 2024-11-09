6234ef4bb3514f4b42df736bb7c266519d5a36c8
gleysonemilio <gleyson.emilio@outlook.com>
2024-11-09 13:29:45 -0300

    feat (): create new Api

--- src/firebase/Api.ts
+++ src/firebase/Api.ts
@@ -1,5 +1,5 @@
 
-import { PagesInterface } from '@/components/SeachBook'
+import { PagesInterface, SubPagesInterface } from '@/components/SeachBook'
 import {
     collection,
     getFirestore,
@@ -21,6 +21,7 @@ const db = getFirestore(firebaseapp)
 const auth = getAuth(firebaseapp)
 const userCollectionRef = collection(db, 'user')
 const pageCollectionRef = collection(db, 'page')
+const subPageCollectionRef = collection(db, 'sub-page')
 const provider = new GoogleAuthProvider()
 
 export interface UpdatePageInterface {
@@ -64,7 +65,14 @@ const getPagesOfUser = async (idUser: string) => {
     return data.docs.map((doc) => ({ ...doc.data(), id: doc.id }))
 }
 
-const getPageId = async (idPage: string) => { //not use
+const getSubPage = async (idPage: string) => {
+    const searchPageUser = query(subPageCollectionRef, where("idPage", "==", idPage));
+    const data = await getDocs(searchPageUser);
+
+    return data.docs.map((doc) => ({ ...doc.data(), id: doc.id }))
+}
+
+const getPageId = async (idPage: string) => {
     const data = await getDoc(doc(db, 'page', idPage))
     return data
 }
@@ -78,20 +86,40 @@ async function createUser(name: string, email: string) {
 }
 
 const createPage = async ({ idUser, title, subtitle, emoji }: PagesInterface) => {
+    if (!title || !idUser) return null
+
     const page = await addDoc(pageCollectionRef, {
         content: '<h1>About what are you thinking?🤔 </h1>',
         idUser: idUser || null,
         title: title || null,
         subtitle: subtitle || null,
-        emoji: emoji || null
+        emoji: emoji || '📑'
     })
 
     return page
 }
 
+const createSubPage = async ({ idPage, title, subtitle, emoji }: SubPagesInterface) => {
+    if (!title || !idPage) return null
+
+    const subPage = await addDoc(subPageCollectionRef, {
+        content: '<h1>About what are you thinking?🤔 </h1>',
+        idPage: idPage || null,
+        title: title || null,
+        subtitle: subtitle || null,
+        emoji: emoji || '📑'
+    })
+
+    return subPage
+}
 
 async function deleterUser(id: string) {
     const userDoc = await doc(db, 'user', id)
+    await deleteDoc(userDoc)
+}
+
+async function deleterPage(id: string) {
+    const userDoc = await doc(db, 'sub-page', id)
     await deleteDoc(userDoc)
 }
 
@@ -107,6 +135,8 @@ async function updateUser(id: string, name: string, email: string) {
 const updatePageOfUser = async ({ id, idUser, title, subtitle, content }: UpdatePageInterface) => {
     const page = doc(db, 'page', id)
 
+    if (!content) return null
+
     await updateDoc(page, {
         content: content || null,
         idUser: idUser || null,
@@ -117,4 +147,4 @@ const updatePageOfUser = async ({ id, idUser, title, subtitle, content }: Update
     return page
 }
 
-export { getUser, createUser, deleterUser, updateUser, getPagesOfUser, getPageId, createPage, updatePageOfUser, signInWithPopupFirebase }
+export { getUser, createUser, deleterUser, updateUser, getPagesOfUser, getPageId, createPage, updatePageOfUser, signInWithPopupFirebase, createSubPage, deleterPage, getSubPage }
