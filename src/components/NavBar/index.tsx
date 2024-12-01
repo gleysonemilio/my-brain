b5da7ae4c55ae6df36473b8637b1a1bd1bc1cc2d
gleysonemilio <gleyson.emilio@outlook.com>
2024-12-01 20:03:06 -0300

    feat (NavBar): fix color NavBar

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -5,7 +5,7 @@ import { SearchBook } from '../SeachBook'
 
 export const NavBar = ({ getUpdatePage }: { getUpdatePage: () => void }) => {
   return (
-    <div className="h-[100vh] border-r-zinc-700 bg-[#0b0b0c] p-3 gap-5">
+    <div className="h-[100%] border-r-zinc-700 bg-[#0b0b0c] p-3 gap-5">
       <button className="flex items-center" onClick={getUpdatePage}>
         <Image src={LogoBrain} width={70} height={30} alt="Picture of the author" />
       </button>
